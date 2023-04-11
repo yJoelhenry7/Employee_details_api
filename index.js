81ef40220e3d32c3f3e8b8fae1de270a28894789
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2023-04-11 11:16:29 +0530

    initialize sequelize

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const sequelize = require('sequelize');
+const cors = require('cors')
+var corsOptions = {
+    origin: "http://localhost:3001"
+    };
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// middlewares
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
  
+// routes
 app.get("/",(request,response)=>{
     response.send("Server is running");
 })
