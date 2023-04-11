f97f534f325c74748fa43127ca72fb6b2435d6d2
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2023-04-11 11:09:03 +0530

    Adding required packages

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("server is running");
+const express = require('express');
+const sequelize = require('sequelize');
+const app = express();
+const PORT = process.env.PORT || 3000;
+ 
+app.get("/",(request,response)=>{
+    response.send("Server is running");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is up! and Running at port ${PORT}`)
+})
